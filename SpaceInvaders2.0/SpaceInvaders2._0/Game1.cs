c482945f43118eca94fa95d3ac01d3ceee4064e1
Absolutelumi <jackson.saylor@yahoo.com>
2016-11-15 22:06:08 -0500

    Updated some erros made in //damage

--- SpaceInvaders2.0/SpaceInvaders2._0/Game1.cs
+++ SpaceInvaders2.0/SpaceInvaders2._0/Game1.cs
@@ -25,41 +25,45 @@ namespace SpaceInvaders2._0
         Texture2D Healthbar;
         SpriteFont Reg;
         SpriteFont gameover;
-        KeyboardState OldKeyState; 
+        SoundEffect milktea;
+
 
         static private Random random = new Random();
 
         static int spaceshipx = 250;
-        
+        int distance = spaceshipx + 100;
+
         int healthx = 350;
         int GameScore = 0;
-        static int Health = 5;
+        int Health = 5;
 
         int damage = 350 / 5; 
  
-        int shootx = spaceshipx;
+        int shootx;
         int shooty = 600;
+        int whenShot; 
 
         bool alive = true;
         bool shoot = false;
-        bool pause = false; 
+        bool pause = false;
+        bool ses = false;
 
         int min = 0;
         float seconds = 0f; 
        
-        int asteroidx1 = random.Next(0, 600);
-        int asteroidx2 = random.Next(0, 600);
-        int asteroidx3 = random.Next(0, 600);
+        static int asteroidx1 = random.Next(0, 600);
+        static int asteroidx2 = random.Next(0, 600);
+        static int asteroidx3 = random.Next(0, 600);
 
-        int asteroidy = random.Next(-200, -50); 
-        int asteroidy2 = random.Next(-200, -50);
-        int asteroidy3 = random.Next(-200, -50);
+        static int asteroidy = random.Next(-200, -50); 
+        static int asteroidy2 = random.Next(-200, -50);
+        static int asteroidy3 = random.Next(-200, -50);
         int asteroidSpeed = 3;
         bool asteroid2 = false;
         bool asteroid3 = false;
 
-        Rectangle Hitbox; 
-        Rectangle Hitbox2;
+        Rectangle Hitbox = new Rectangle(asteroidx1, asteroidy, 100, 100);
+        Rectangle Hitbox2 = new Rectangle(asteroidx2, asteroidy2, 100, 100); 
         Rectangle Hitbox3;
         #endregion
 
@@ -77,6 +81,7 @@ namespace SpaceInvaders2._0
             graphics.ApplyChanges();
             IsMouseVisible = false;
             IsFixedTimeStep = false;
+            
 
             base.Initialize();
         }
@@ -90,9 +95,9 @@ namespace SpaceInvaders2._0
             asteroid = Content.Load<Texture2D>("rock4_b");
             Healthbar = Content.Load<Texture2D>("Healthbar");
             laser = Content.Load<Texture2D>("Red_laser");
+            milktea = Content.Load<SoundEffect>("milk");
+            milktea.Play(); 
             spriteBatch = new SpriteBatch(GraphicsDevice);
-
-
         }
 
         protected override void UnloadContent()
@@ -102,8 +107,8 @@ namespace SpaceInvaders2._0
 
         protected override void Update(GameTime gameTime)
         {
-            KeyboardState NewKeyState = Keyboard.GetState(); 
-
+            KeyboardState NewKeyState = Keyboard.GetState();
+            
             // Movement
             if (Keyboard.GetState().IsKeyDown(Keys.Right))
             {
@@ -117,7 +122,8 @@ namespace SpaceInvaders2._0
             // Shooting
             if (Keyboard.GetState().IsKeyDown(Keys.Space))
             {
-                shoot = true;
+                DateTime Fired = DateTime.Now; 
+                shoot = true;  
             }
             if (shooty <= -25)
             {
@@ -139,16 +145,21 @@ namespace SpaceInvaders2._0
                 asteroidy3 = random.Next(-200, -50);
                 GameScore++; 
             }
-            if(shoot == true)
+            if (ses == true)
             {
-                shootx = spaceshipx + 40;  
+                whenShot = spaceshipx + 40; 
+            }
+            if (shoot == true)
+            {
+                shootx = whenShot;
                 shooty = shooty - 9;
             }
+            
 
             // Pause
             if (Keyboard.GetState().IsKeyDown(Keys.Escape))
             {
-                pause = true; 
+                pause = true;
                 this.Exit(); 
             }
 
@@ -170,14 +181,50 @@ namespace SpaceInvaders2._0
                 asteroidy = -50;
                 asteroidx1 = random.Next(0, 600);
             }
-            if(Health <= 0)
+            if (asteroidy2 >= 800)
             {
-                alive = false;
-                
+                Health = Health - 1;
+                healthx = healthx - damage;
+                asteroidy2 = -50;
+                asteroidx2 = random.Next(0, 600);
+            }
+            if (asteroidy3 >= 800)
+            {
+                Health = Health - 1;
+                healthx = healthx - damage;
+                asteroidy3 = -50;
+                asteroidx3 = random.Next(0, 600);
+            }
+
+            if (Health <= 0)
+            {
+                alive = false; 
+            }
+
+            if (spaceshipx <= asteroidx1 && distance >= asteroidx1 && asteroidy >= 700)
+            {
+                asteroidy = random.Next(-200, -50);
+                asteroidx1 = random.Next(0, 600);
+                Health = Health - 2;
+                healthx = healthx - damage * 2;
+            }
+            if (spaceshipx <= asteroidx2 && distance >= asteroidx2 && asteroidy2 >= 700)
+            {
+                asteroidy2 = random.Next(-200, -50);
+                asteroidx2 = random.Next(0, 600);
+                Health = Health - 2;
+                healthx = healthx - damage * 2;
+            }
+            if (spaceshipx <= asteroidx3 && distance >= asteroidx3 && asteroidy3 >= 700)
+            {
+                asteroidy3 = random.Next(-200, -50);
+                asteroidx3 = random.Next(0, 600);
+                Health = Health - 2;
+                healthx = healthx - damage * 2;
             }
 
             // Time
-            if(seconds >= 60)
+            if (seconds >= 60)
             {
                 min++;
                 seconds = 0; 
@@ -196,12 +243,18 @@ namespace SpaceInvaders2._0
                 asteroidSpeed = asteroidSpeed + 1;
             }
 
+            // Retry
+            if (alive == false)
+            {
+                
+            }
+
             // Asteroid Transformations
             asteroidy = asteroidy + asteroidSpeed;
             asteroidy2 = asteroidy2 + asteroidSpeed;
             asteroidy3 = asteroidy3 + asteroidSpeed; 
 
-            // Other
+            // Time
             seconds += (float)gameTime.ElapsedGameTime.TotalSeconds;
             
 
@@ -210,8 +263,9 @@ namespace SpaceInvaders2._0
 
         protected override void Draw(GameTime gameTime)
         {
-            Texture2D[] Bullets = new Texture2D[5];  
-            for(int i = 0; i < 5; i++)
+            Texture2D[] Bullets = new Texture2D[5];
+            
+            for (int i = 0; i < 5; i++)
             {
                 Bullets[i] = laser; 
             }
@@ -222,8 +276,12 @@ namespace SpaceInvaders2._0
             Hitbox2 = new Rectangle(asteroidx2, asteroidy2, 100, 100);
             Hitbox3 = new Rectangle(asteroidx3, asteroidy3, 100, 100);
             spriteBatch.Begin();
+
+            // Main
             spriteBatch.Draw(background, mainFrame, Color.White);
             spriteBatch.Draw(spaceship, new Rectangle(spaceshipx, 700, 100, 100), Color.White);
+
+            // Asteroid
             spriteBatch.Draw(asteroid, new Rectangle(asteroidx1, asteroidy, 100, 100), Color.White);
             if(asteroid2 == true)
             {
@@ -233,16 +291,12 @@ namespace SpaceInvaders2._0
             {
                 spriteBatch.Draw(asteroid, new Rectangle(asteroidx3, asteroidy3, 100, 100), Color.White);
             }
+
+            // UI
             spriteBatch.Draw(Healthbar, new Rectangle(0, 0, healthx, 25), Color.White);
-            if(seconds  < 10)
-            {
-                spriteBatch.DrawString(Reg, min + ":" + 0 + seconds.ToString(), new Vector2(580, 150), Color.White, 0, FontOrigin, 1.0f, SpriteEffects.None, 0.5f);
-            }
-            else
-            {
-                spriteBatch.DrawString(Reg, min + ":" + seconds.ToString(), new Vector2(580, 150), Color.White, 0, FontOrigin, 1.0f, SpriteEffects.None, 0.5f);
-            }
-            
+
+            spriteBatch.DrawString(Reg, gameTime.ElapsedGameTime.ToString("mmss"), new Vector2(580, 150), Color.White, 0, FontOrigin, 1.0f, SpriteEffects.None, 0.5f);
+
             if (shoot == true)
             {
                 for (int i = 0; i < 5; i++)
@@ -251,10 +305,17 @@ namespace SpaceInvaders2._0
                 }
             }
             spriteBatch.DrawString(Reg, GameScore.ToString(), new Vector2(620, 50), Color.White, 0, FontOrigin, 1.0f, SpriteEffects.None, 0.5f);
+            if(pause == false)
+            {
+
+            }
             if (alive == false)
             {
                 spriteBatch.Draw(background, new Rectangle(0, 0, 700, 800), Color.Black);
-                spriteBatch.DrawString(gameover, "Game Over", new Vector2(150, 400), Color.White, 0, FontOrigin, 1.0f, SpriteEffects.None, 0.5f);
+                spriteBatch.DrawString(gameover, "Game Over", new Vector2(150, 200), Color.White, 0, FontOrigin, 1.0f, SpriteEffects.None, 0.5f);
+                spriteBatch.DrawString(Reg, "Retry?", new Vector2(280, 350), Color.White, 0, FontOrigin, 1.0f, SpriteEffects.None, 0.5f);
+                spriteBatch.DrawString(Reg, "Yes", new Vector2(250, 500), Color.White, 0, FontOrigin, 1.0f, SpriteEffects.None, 0.5f);
+                spriteBatch.DrawString(Reg, "No", new Vector2(450, 500), Color.White, 0, FontOrigin, 1.0f, SpriteEffects.None, 0.5f);
             }
             spriteBatch.End();
             base.Draw(gameTime);
